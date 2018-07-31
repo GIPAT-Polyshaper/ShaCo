5d4da8be93ed813e427f86b528d4675efdb66716
Tomassino Ferrauto <t_ferrauto@yahoo.it>
2018-07-31 12:41:44 +0200

    Decreased send character delay

--- core/serialport.cpp
+++ core/serialport.cpp
@@ -3,7 +3,7 @@
 #include <QThread>
 
 namespace {
-    const unsigned long charSendDelayMs = 1;
+    const unsigned long charSendDelayUs = 250;
 }
 
 SerialPortInterface::SerialPortInterface()
@@ -34,7 +34,7 @@ qint64 SerialPort::write(const QByteArray& data)
     // Suggestion taken from GrblController (https://github.com/zapmaker/GrblHoming/blob/master/rs232.cpp
     // at row 180): "On very fast PCs running Windows we have to slow down the sending of bytes to grbl
     // because grbl loses bytes due to its interrupt service routine (ISR) taking too many clock
-    // cycles away from serial handling.". Here we send one byte at a time with an small sleep
+    // cycles away from serial handling.". Here we send one byte at a time with an small delay
     qint64 retval = 0;
     for (int i = 0; i < data.size(); ++i) {
         auto r = m_serialPort.write(&(data.data()[i]), 1);
@@ -48,7 +48,7 @@ qint64 SerialPort::write(const QByteArray& data)
             ++retval;
         }
 
-        QThread::msleep(charSendDelayMs);
+        QThread::usleep(charSendDelayUs);
     }
 
     return retval;
